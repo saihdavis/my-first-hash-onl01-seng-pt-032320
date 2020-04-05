c319ea5c6e03bf88dc9c16702a5f2a2ae22c06a1
Saida Davis <saihdavis@gmail.com>
2020-04-05 16:18:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,14 +25,9 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+ shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
+  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest
 
 end
